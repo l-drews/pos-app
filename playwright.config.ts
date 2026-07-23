20d0b65fa32c2b627da4969c07bb1569a59ee504
Lukas Drews <ldrews@2wcom.com>
2026-07-23 10:57:25 +0200

    chore: move tests/e2e to e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./tests/e2e/specs",
+  testDir: "./e2e/specs",
   timeout: 30_000,
   retries: 0,
   use: {
